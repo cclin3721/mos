f91f9c09941adad50d153c0436c744bdc145ac1d
cclin3721 <cclin3721@gmail.com>
2021-01-06 17:30:11 +0800

    deploy

---
+++ precache-manifest.2c312d1f1a8f576dfadc98804c39d75e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e7bee88dd5d48107c46",
+    "url": "/mos/css/app.851ae1fd.css"
+  },
+  {
+    "revision": "7e7bee88dd5d48107c46",
+    "url": "/mos/js/app.645118bb.js"
+  },
+  {
+    "revision": "9de133eaf33ece1b8129",
+    "url": "/mos/css/chunk-vendors.d4d47c82.css"
+  },
+  {
+    "revision": "9de133eaf33ece1b8129",
+    "url": "/mos/js/chunk-vendors.b97043b7.js"
+  },
+  {
+    "revision": "5ffac0527916ba92690bd81b949fb703",
+    "url": "/mos/index.html"
+  },
+  {
+    "revision": "6a47843888fe188b9edba11f4b0f9a17",
+    "url": "/mos/img/bot.gif"
+  },
+  {
+    "revision": "d0aa14e8ed3363b70e50476f2138b54c",
+    "url": "/mos/img/bot.png"
+  },
+  {
+    "revision": "391e4fb8f4a4ec32c32ca20a566219a9",
+    "url": "/mos/img/legend.png"
+  },
+  {
+    "revision": "79a02992d8bdeff8c1b9a7e408a28bfc",
+    "url": "/mos/img/mitac_logo02.png"
+  },
+  {
+    "revision": "2cb4594fe5207412a8d15d751cf0e578",
+    "url": "/mos/img/type-magnified-thumb.jpg"
+  },
+  {
+    "revision": "54c697147c04fbc21bb150d77bd08da0",
+    "url": "/mos/img/type-microscope-thumb.jpg"
+  },
+  {
+    "revision": "8e73b36270e282252a67944140bd53b8",
+    "url": "/mos/img/Screenshot-Descriptions.png"
+  },
+  {
+    "revision": "15664c7236991e795991aad4555b64ff",
+    "url": "/mos/img/type-microscope.jpg"
+  },
+  {
+    "revision": "20f7377552ea1611d841d097a87993b8",
+    "url": "/mos/img/type-paper-thumb.jpg"
+  },
+  {
+    "revision": "d3b374f148f42e909837a960a69a080a",
+    "url": "/mos/img/type-paper1.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/mos/robots.txt"
+  },
+  {
+    "revision": "068de0ecaf12ed0e41c933a56fd3433e",
+    "url": "/mos/img/type-magnified.jpg"
+  },
+  {
+    "revision": "0343869b3b734d75c030b09a571948f8",
+    "url": "/mos/img/type-paper.jpg"
+  },
+  {
+    "revision": "0610f7d03713bda645888f03bb7600bf",
+    "url": "/mos/img/type-paper2.jpg"
+  },
+  {
+    "revision": "1f5362cb0f8d4eab1f10205ae869a453",
+    "url": "/mos/opencv.js"
+  }
+];
